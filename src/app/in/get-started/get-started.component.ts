bbc7233450bd0004de834dd8cebb773a2e180fbe
asitjavarnd <asit.javarnd@gmail.com>
2023-01-25 09:38:57 -0500

    signup proper routing

--- src/app/in/get-started/get-started.component.ts
+++ src/app/in/get-started/get-started.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-get-started',
   templateUrl: './get-started.component.html',
-  styleUrls: ['./get-started.component.scss']
+  styleUrls: ['./get-started.component.scss'],
 })
 export class GetStartedComponent implements OnInit {
   panelOpenState = false;
-  isClicked= false;
+  isClicked = false;
 
-  constructor() { }
+  constructor(private _router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   executeUsecase() {
     this.isClicked = true;
+  }
+
+  goToSignUpPage() {
+    this._router.navigate(['signup']);
   }
 }
--- src/app/signup/registration/registration.component.ts
+++ src/app/signup/registration/registration.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  constructor(private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToRegFormPage() {
+    this._router.navigate(['signup/regform']);
   }
-
 }
