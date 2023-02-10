9a37a54b70aa521da545265f9758d88bf29042ba
asitjavarnd <asit.javarnd@gmail.com>
2023-02-10 16:46:12 -0500

    router hashing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,8 +16,7 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomeModule),
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
     canActivate: [AuthGuard],
   },
   {
@@ -28,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
