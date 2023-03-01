9ee3c42cbeb72ec979265d6f01af11ea04520cb4
Brajesh <maharana.brajesh@gmail.com>
2023-03-02 03:49:45 +0530

    plan_form completed

--- src/app/signup/plan-form/plan-form.component.ts
+++ src/app/signup/plan-form/plan-form.component.ts
@@ -1,6 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface DevicesData {
+  name: string;
+  price: string;
+  quality: string;
+  resolution: string;
+  devices_mobile: string;
+  devices_tablet: string;
+  devices_laptop: string;
+  devices_tv: string;
+
+}
+
+const data_Device: DevicesData[] = [
+  {name: "Mobile", price: '₹ 149', quality: "Good", resolution: '480p', devices_mobile: 'phone_iphone', devices_tablet: 'tablet', devices_laptop: '', devices_tv: ''},
+  {name: "Basic", price: '₹ 199', quality: "Good", resolution: '720p', devices_mobile: 'phone_iphone', devices_tablet: 'tablet', devices_laptop: 'laptop_mac', devices_tv: 'tv'},
+  {name: "Standard", price: '₹ 499', quality: "Better", resolution: '1080p', devices_mobile: 'phone_iphone', devices_tablet: 'tablet', devices_laptop: 'laptop_mac', devices_tv: 'tv'},
+  {name: "Premium", price: '₹ 649', quality: "Best", resolution: '4K+HDR', devices_mobile: 'phone_iphone', devices_tablet: 'tablet', devices_laptop: 'laptop_mac', devices_tv: 'tv'},
+];
+
 @Component({
   selector: 'app-plan-form',
   templateUrl: './plan-form.component.html',
@@ -8,14 +27,15 @@ import { Router } from '@angular/router';
 })
 export class PlanFormComponent implements OnInit {
 selectedPlan = 2;
+  price_flows: any
   constructor(private _router: Router) {}
 
   ngOnInit(): void {
+    this.price_flows = data_Device
   }
   goToRegFormPage() {
     this._router.navigate(['signup/payment-picker']);
   }
-
   selectPlan(plannumber:number){
     this.selectedPlan=plannumber;
   }
