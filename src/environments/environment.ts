662de07869ef21beb4a30cfa0ebd029087939de3
asitjavarnd <asit.javarnd@gmail.com>
2023-03-13 12:38:30 -0400

    home page api updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseURL: 'http://3.111.212.219:3004/api/v1/',
+  baseURL:
+    'http://cbmpltech-dev-ui-lb-918274883.ap-south-1.elb.amazonaws.com:3004/api/v1/',
   cognito: {
     userPoolId: 'ap-south-1_SnceAtMEQ',
     userPoolWebClientId: '7jiv5r1dgq2tlkngh80lnom8ak',
