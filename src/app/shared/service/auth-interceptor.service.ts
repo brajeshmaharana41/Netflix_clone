315136fc881895e2132c435b3e2f381c713af50b
AkashTechNest <technest.itservices@gmail.com>
2023-05-30 20:06:18 +0530

    Logout user if any API returns 401 in response.

--- src/app/shared/service/auth-interceptor.service.ts
+++ src/app/shared/service/auth-interceptor.service.ts
@@ -4,16 +4,20 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
+  HttpResponse,
 } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
 import { Constants } from '../constants/constant';
 // import { Constants } from '../common/constant';
+import {map} from 'rxjs/operators';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(private router: Router) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -27,11 +31,19 @@ export class AuthInterceptorService implements HttpInterceptor {
         hgdh: 'hgj',
         Pragma: 'no-cache',
         Authorization: `Bearer ${localStorage.getItem(Constants.SESSIONTOKENSTRING)}`
-         
+
       },
     };
 
     const newRequest = req.clone(config);
-    return next.handle(newRequest);
+    return next.handle(newRequest).pipe(map((event: HttpEvent<any>) => {
+      if (event instanceof HttpResponse) {
+         if(event?.status == 401) {
+          localStorage.clear();
+          this.router.navigate(['/']);
+         }
+      }
+      return event;
+  }));;
   }
 }
